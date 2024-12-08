9399ad14d856f6b19626a8ce8c1c20c032325934
unknown <vikram12.github@gmail.com>
2024-12-08 19:47:25 +0530

    removing guest login due to issue in login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -90,12 +90,12 @@ export const Login = () => {
           Log In
         </button>
       </form>
-      <button
+      {/* <button
         onClick={handleGuestLogin}
         className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       >
         Login As Guest
-      </button>
+      </button> */}
     </main>
   );
 };
